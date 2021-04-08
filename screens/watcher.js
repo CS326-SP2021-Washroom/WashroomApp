0aebc31c1042cad1cd88ca227ef6d57594742186
andrewJamesBaker <61355438+andrewJamesBaker@users.noreply.github.com>
2021-04-08 17:22:18 -0400

    worked on adding mqtt to the app

--- screens/watcher.js
+++ screens/watcher.js
@@ -1,17 +1,51 @@
 import React, { useState, useEffect } from 'react';
 import {
     ImageBackground, StyleSheet, View, Text, TouchableOpacity, TouchableWithoutFeedback, Keyboard,
-    TextInput, Alert
+    TextInput, Alert, AsyncStorage
 } from 'react-native';
+
+// import { AsyncStorage } from '@react-native-community/async-storage'
 import Card from '../components/card';
 import { globalStyles } from '../components/globalStyle'
 import CountDown from 'react-native-countdown-component'
 import { FlatList } from 'react-native-gesture-handler';
 import Styler from '../components/styler'
+import * as Mqtt from 'react-native-native-mqtt'
+import init from "react_native_mqtt"
 
 export default function Watcher({ navigation }) {
 
-    const [washers, setWashers] = useState([
+    init({
+        size: 10000,
+        storageBackend: AsyncStorage,
+        defaultExpires: 1000 * 3600 * 24,
+        enableCache: true,
+        reconnect: true,
+        sync : {
+        }
+      });
+      
+      function onConnect() {
+        console.log("onConnect");
+      }
+      
+      function onConnectionLost(responseObject) {
+        if (responseObject.errorCode !== 0) {
+          console.log("onConnectionLost:"+responseObject.errorMessage);
+        }
+      }
+      
+      function onMessageArrived(message) {
+        console.log("onMessageArrived:"+message.payloadString);
+      }
+      
+      const client = new Paho.MQTT.Client('test.mosquitto.org', 1883, 'ajb247');
+      client.onConnectionLost = onConnectionLost;
+      client.onMessageArrived = onMessageArrived;
+      client.connect({ onSuccess:onConnect, useSSL: true });
+
+
+    const [washers, setWashers] = useState([            //Needs to be renamed, does not reference the apartments
         { title: 'Alpha', key: '1' },
         { title: 'Beta', key: '2' },
         { title: 'Delta', key: '3' },
