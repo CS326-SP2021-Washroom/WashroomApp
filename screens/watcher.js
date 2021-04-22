ebebc3b713f7cc36d9103de577b91c3240461e7b
andrewJamesBaker <61355438+andrewJamesBaker@users.noreply.github.com>
2021-04-22 12:10:47 -0400

    FINALLY GOT MQTT WORKINGgit add .!

--- screens/watcher.js
+++ screens/watcher.js
@@ -5,76 +5,26 @@ import { globalStyles } from '../components/globalStyle'
 import CountDown from 'react-native-countdown-component'
 import { FlatList } from 'react-native-gesture-handler';
 import Styler from '../components/styler'
+import Paho from '../components/paho-mqtt'
 
 export default function Watcher({ navigation }) {
-    // ##########################################################################
 
-
-    // ################################################################################
-    // const client = new Mqtt.Client('mqtt://iot.cs.calvin.edu:8883');
-    // client.connect({
-    //     clientId: 'ajb247/app',
-    //     username: 'cs326',
-    //     password: 'piot'
-    // }, err => {});
-
-    // client.on(Mqtt.Event.Message, (topic, message) => {
-    //     console.log('Mqtt Message:', topic, message.toString());
-    // });
-
-    // client.on(Mqtt.Event.Connect, () => {
-    //     console.log('MQTT Connect');
-    //     client.subscribe(['ajb247/app'], [0]);
-    // });
-
-    // client.on(Mqtt.Event.Error, (error) => {
-    //     console.log('MQTT Error:', error);
-    // });
-
-    // client.on(Mqtt.Event.Disconnect, (cause) => {
-    //     console.log('MQTT Disconnect:', cause);
-    // });
-    // ################################################################################
-
-    // var mqtt = require('mqtt')
-    // var client = mqtt.connect({host: 'test.mosquitto.org'})
-
-    // client.on('connect', function () {
-    //     client.subscribe('presence', function (err) {
-    //         if (!err) {
-    //             client.publish('presence', 'Hello mqtt')
-    //         }
-    //     })
-    // })
-
-    // client.on('message', function (topic, message) {
-    //     // message is Buffer
-    //     console.log(message.toString())
-    //     client.end()
-    // })
-
-    // #################################################################################
-
-    var ascoltatori = require('ascoltatori');
-    settings = {
-        type: 'mqtt',
-        json: false,
-        mqtt: require('mqtt'),
-        url: 'mqtt://test.mosquitto.org:1883'
+    client = new Paho.Client(host = 'mqtt.eclipseprojects.io/mqtt', port = 80, clientId = 'washroom');
+    client.onMessageArrived = onMessageArrived;
+    var options = {
+        useSSL: false,
+        keepAliveInterval: 60,
+        onSuccess: onConnect,
     };
-
-    ascoltatori.build(settings, function (err, ascoltatore) {
-        // subscribes to a topic
-        ascoltatore.subscribe('hello', function () {
-            console.log(arguments);
-            // { '0': 'hello', '1': 'a message' }
-        });
-
-        // publishes a message to the topic 'hello'
-        ascoltatore.publish('hello', 'a message', function () {
-            console.log('message published');
-        });
-    });
+    client.connect(options);
+    function onConnect() {
+        console.log("Connected!");
+        client.subscribe("cs326/message");
+    }
+    function onMessageArrived(message) {
+        console.log("Message Arrived:" + message.payloadString);
+        document.getElementById("msg").innerHTML = message.payloadString;
+    }
 
     const [washers, setWashers] = useState([            //Needs to be renamed, does not reference the apartments
         { title: 'Washer 1:', key: '1', test: true },
@@ -85,6 +35,7 @@ export default function Watcher({ navigation }) {
 
     return (
         <Styler>
+            {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.1.0/paho-mqtt.js" type="text/javascript"></script> */}
 
             <View style={globalStyles.containerAcross}>
                 <View style={globalStyles.container}>
